123f9264e24376fa74a249934c9e0f53b1549f82
Ani Tukvadze <ani.tukvadze14@geolab.edu.ge>
2021-12-03 09:32:14 -0800

    added new task

---
+++ main.js
@@ -0,0 +1,24 @@
+
+let participants = [{
+    name: 'ani',
+    age: 25
+},
+{
+    name: 'mari',
+    age: 17
+},
+{
+    name: 'shota',
+    age: 27
+},
+{
+    name: 'gio',
+    age: 16
+}];
+document.getElementById("test").innerHTML = participants.map(e => e.name ).join(', ');
+
+
+function myFunction() {
+let result = participants.filter(participant => participant.age > 18);
+document.getElementById("test").innerHTML = result.map(e => e.name ).join(', ');
+}
